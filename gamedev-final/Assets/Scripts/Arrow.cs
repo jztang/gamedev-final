a66f93b9da11cb963d5f313bbc2d3589f166de4a
unknown <jztang8@gmail.com>
2019-04-23 16:36:06 -0400

    Work on arrow spawning and movement

---
+++ gamedev-final/Assets/Scripts/Arrow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour {
+	private Vector3 startPos;
+	private Vector3 endPos = new Vector3(0f, 0f, -2f);
+	private float startTime;
+	private float journeyLength;
+
+	public float speed = 1f;
+
+    private void Start() {
+        
+    }
+
+    private void Update() {
+        float distCovered = (Time.time - startTime) * speed;
+        float fracJourney = distCovered / journeyLength;
+        transform.position = Vector3.Lerp(startPos, endPos, fracJourney);
+    }
+
+    public void spawnUp() {
+    	transform.position = new Vector3(0f, 6f, -2f);
+        transform.eulerAngles = new Vector3(0f, 0f, 270f);
+    }
+
+    public void shootArrow() {
+
+    }
+}
--- gamedev-final/Assets/Scripts/ArrowSpawner.cs
+++ gamedev-final/Assets/Scripts/ArrowSpawner.cs
@@ -3,11 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ArrowSpawner : MonoBehaviour {
-    void Start() {
+	public GameObject arrowPrefab;
+
+	private Vector3 upSpawnPos = new Vector3(0f, 6f, -2f);
+	private Vector3 upSpawnRot = new Vector3(0f, 0f, 270f);
+	private Vector3 downSpawnPos = new Vector3(0f, -6f, -2f);
+	private Vector3 downSpawnRot = new Vector3(0f, 0f, 90f);
+	private Vector3 leftSpawnPos = new Vector3(-6f, 0f, -2f);
+	private Vector3 leftSpawnRot = new Vector3(0f, 0f, 0f);
+	private Vector3 rightSpawnPos = new Vector3(6f, 0f, -2f);
+	private Vector3 rightSpawnRot = new Vector3(0f, 0f, 180f);
+
+    private void Start() {
+        GameObject up = Instantiate(arrowPrefab);
+        up.transform.position = upSpawnPos;
+        up.transform.eulerAngles = upSpawnRot;
+
+        GameObject down = Instantiate(arrowPrefab);
+        down.transform.position = downSpawnPos;
+        down.transform.eulerAngles = downSpawnRot;
+
+        GameObject left = Instantiate(arrowPrefab);
+        left.transform.position = leftSpawnPos;
+        left.transform.eulerAngles = leftSpawnRot;
+
+        GameObject right = Instantiate(arrowPrefab);
+        right.transform.position = rightSpawnPos;
+        right.transform.eulerAngles = rightSpawnRot;
+    }
+
+    private void Update() {
         
     }
 
-    void Update() {
-        
-    }
+
 }
