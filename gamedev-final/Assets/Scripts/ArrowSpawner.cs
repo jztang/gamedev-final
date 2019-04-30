ec1480417c1e7a944ade688c728ebb03f175aba7
unknown <jztang8@gmail.com>
2019-04-30 14:16:11 -0400

    Update paddle/heart/bg sprites

--- gamedev-final/Assets/Scripts/ArrowSpawner.cs
+++ gamedev-final/Assets/Scripts/ArrowSpawner.cs
@@ -7,13 +7,17 @@ public class ArrowSpawner : MonoBehaviour {
     public float arrowSpeed;
     public float bpm;
 
-    private Vector3[] spawnPos = {new Vector3(0f, 6f, 0f), new Vector3(0f, -6f, 0f), 
-                                  new Vector3(-6f, 0f, 0f), new Vector3(6f, 0f, 0f)};
+    private Vector3[] spawnPos = {new Vector3(0.1f, 6f, 1f), new Vector3(0.1f, -6f, 1f), 
+                                  new Vector3(-6f, 0f, 1f), new Vector3(6f, 0f, 1f)};
     private Vector3[] spawnRot = {new Vector3(0f, 0f, 180f), new Vector3(0f, 0f, 0f),
                                   new Vector3(0f, 0f, 270f), new Vector3(0f, 0f, 90f)};
 
     private void Start() {
         InvokeRepeating("StartArrows", 3f, bpm);
+    }
+
+    private void bpmToFreq(float bpm) { // beats per minute -> seconds per arrow
+
     }
 
     private void StartArrows() {
--- gamedev-final/Assets/Scripts/Paddle.cs
+++ gamedev-final/Assets/Scripts/Paddle.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Paddle : MonoBehaviour {
-	private Vector3 paddleUpPos = new Vector3(0f, 1.22f, -1f);
+	private Vector3 paddleUpPos = new Vector3(0.035f, 0.595f, 0f);
 	private Vector3 paddleUpRot = new Vector3 (0f, 0f, 0f);
-	private Vector3 paddleDownPos = new Vector3(0f, -1.22f, -1f);
+	private Vector3 paddleDownPos = new Vector3(0.027f, -0.595f, 0f);
 	private Vector3 paddleDownRot = new Vector3(0f, 0f, 180f);
-	private Vector3 paddleLeftPos = new Vector3(-1.22f, 0f, -1f);
+	private Vector3 paddleLeftPos = new Vector3(-0.557f, 0.006f, 0f);
 	private Vector3 paddleLeftRot = new Vector3(0f, 0f, 90f);
-	private Vector3 paddleRightPos = new Vector3(1.22f, 0f, -1f);
+	private Vector3 paddleRightPos = new Vector3(0.616f, -0.005f, 0f);
 	private Vector3 paddleRightRot = new Vector3(0f, 0f, 270f);
 
     public GameObject heart;
